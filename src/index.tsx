e66a285e8271552106f254c4ebf50a43a8b392b4
Victor A <victor@namespace.ee>
2020-05-05 00:02:22 +0300

    store state

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-import {Provider} from 'react-redux';
-import { createStore } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import reducers from './reducers';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import reducers from "./reducers";
 
-const store = createStore(reducers, composeWithDevTools())
+const loadState = () => {
+  try {
+    const savedState = localStorage.getItem("swift");
+    return JSON.parse(savedState as string) || undefined;
+  } catch (e) {
+    return undefined;
+  }
+};
+
+const saveSate = (state: any) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem("swift", serializedState);
+  } catch (e) {}
+};
+
+const initialState = loadState();
+const store = createStore(reducers, initialState, composeWithDevTools());
+store.subscribe(() => {
+  saveSate(store.getState());
+});
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+      <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
