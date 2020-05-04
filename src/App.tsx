9b920d3762cc3c3f94dfe2e6b353cb73920b2412
Victor A <victor@namespace.ee>
2020-05-04 21:56:16 +0300

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import {Button} from "antd";
+
+function App() {
+  return (
+    <div className="App">
+        <Button type={'primary'}>Add User</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducers/index.ts
@@ -0,0 +1,5 @@
+import {createReducer} from "@reduxjs/toolkit";
+
+export default createReducer({}, {
+
+});
